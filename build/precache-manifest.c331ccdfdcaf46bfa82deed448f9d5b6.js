482291be8d439e532d2e3d2a4b9897275afa2ce7
Atul Anand <62374512+Atul0419-223@users.noreply.github.com>
2020-09-09 12:57:34 +0530

    Add files via upload

---
+++ build/precache-manifest.c331ccdfdcaf46bfa82deed448f9d5b6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f614f84a68cddc9543500165d8113cd6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "caa21b386b23c20547d3",
+    "url": "/static/css/2.6df4c8f8.chunk.css"
+  },
+  {
+    "revision": "954f71aecbc4df10b1a0",
+    "url": "/static/css/main.675581eb.chunk.css"
+  },
+  {
+    "revision": "caa21b386b23c20547d3",
+    "url": "/static/js/2.497dcc99.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.497dcc99.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "954f71aecbc4df10b1a0",
+    "url": "/static/js/main.ad353b9b.chunk.js"
+  },
+  {
+    "revision": "09b16c74d82e720ad543",
+    "url": "/static/js/runtime-main.6b002cdf.js"
+  }
+]);
